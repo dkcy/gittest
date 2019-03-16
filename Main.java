27428a30b70fb0c3d51d27bd5147b4f5c1d7eee5
Daniel Kim <dk@Daniels-MacBook-Pro.local>
2019-03-16 17:47:33 -0400

    Adding node to test1_branch

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test1!");
+        System.out.println("On Test1 Branch");
+
+        Node head = new Node(2);
+        Node node = new Node(3);
+        Node node1 = new Node(4);
+
+        head.next = node;
+        node.next = node1;
     }
 }
